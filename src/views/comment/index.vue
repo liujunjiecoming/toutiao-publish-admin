b54833259d2329cc2a787f4e5938e17ac5a8e929
JJLiu <13431967860@163.com>
2020-11-22 16:52:26 +0800

    1

---
+++ src/views/comment/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="comment-container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>评论管理</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <div>
+        <el-table
+          :data="articles"
+          style="width: 100%"
+          stripe
+          class="table-list"
+          >
+          <el-table-column
+            prop="title"
+            label="标题">
+          </el-table-column>
+          <el-table-column
+            prop="total_comment_count"
+            label="总评论数">
+          </el-table-column>
+          <el-table-column
+            prop="fans_comment_count"
+            label="粉丝评论数">
+          </el-table-column>
+          <el-table-column
+            prop="address"
+            label="状态">
+            <template slot-scope="scope">
+              {{ scope.row.comment_status ? '正常' : '关闭' }}
+            </template>
+          </el-table-column>
+          <el-table-column
+            prop="address"
+            label="操作">
+            <template slot-scope="scope">
+              <el-switch
+                v-model="scope.row.comment_status"
+                active-color="#13ce66"
+                inactive-color="#ff4949"
+                @change="onStatusChange(scope.row)"
+                >
+              </el-switch>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="1"
+          :page-sizes="[100, 200, 300, 400]"
+          :page-size="100"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="400"
+          background>
+        </el-pagination>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import { getArticle } from '@/api/article'
+
+export default {
+  name: 'CommentIndex',
+  data () {
+    return {
+      articles: [] // 评论列表
+    }
+  },
+  created () {
+    this.loadComments()
+  },
+  methods: {
+    handleSizeChange () { },
+    handleCurrentChange () { },
+    loadComments () {
+      getArticle({
+        response_type: 'comment'
+      }).then(res => {
+        this.articles = res.data.data.results
+      })
+    },
+    onStatusChange (article) {
+      console.log(article)
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.table-list {
+  margin-bottom: 30px;
+}
+</style>
